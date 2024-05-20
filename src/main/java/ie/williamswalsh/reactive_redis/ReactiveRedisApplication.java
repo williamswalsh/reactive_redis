813f7485383f319f07ddb32fdfff82a25ca95ee0
William Walsh <william.stephen.walsh@gmail.com>
2024-05-20 15:44:39 +1000

    Add all files.

---
+++ src/main/java/ie/williamswalsh/reactive_redis/ReactiveRedisApplication.java
@@ -0,0 +1,13 @@
+package ie.williamswalsh.reactive_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/williamswalsh/reactive_redis/ReactiveRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.williamswalsh.reactive_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
